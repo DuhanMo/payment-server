6953fe4340ae20e618bc6ac8342cece50c325a43
Duhan Mo <duhandv@gmail.com>
2024-09-18 22:15:23 +0900

    first-commit

---
+++ src/main/kotlin/org/duhan/webfluxcoroutinepaymentserver/WebfluxCoroutinePaymentServerApplication.kt
@@ -0,0 +1,11 @@
+package org.duhan.webfluxcoroutinepaymentserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxCoroutinePaymentServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxCoroutinePaymentServerApplication>(*args)
+}
---
+++ src/test/kotlin/org/duhan/webfluxcoroutinepaymentserver/WebfluxCoroutinePaymentServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.duhan.webfluxcoroutinepaymentserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxCoroutinePaymentServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
