1497b1c432f022477fa78db01fcf2ef822b9eea7
Duhan Mo <duhandv@gmail.com>
2024-10-19 18:04:08 +0900

    refactor: 미사용 도메인 제거

--- src/main/kotlin/org/duhan/webfluxcoroutinepaymentserver/article/ArticleRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.duhan.webfluxcoroutinepaymentserver.article
-
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ArticleRepository : CoroutineCrudRepository<Article, Long>
