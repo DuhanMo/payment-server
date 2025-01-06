8b4224a37b643810ccc1a8cfc50293f30ea6f375
Duhan Mo <duhandv@gmail.com>
2025-01-06 22:58:37 +0900

    [front] feat: add tosspayment widget

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import ProductList from './components/ProductList';
-import OrderForm from './components/OrderForm';
 import './App.css';
+import {CheckoutPage} from "./components/Checkout";
 
-const App = () => {
+function App() {
     return (
-        <div className="App">
-            <header>
-                <h1>주문 관리 시스템</h1>
-            </header>
-            <main>
-                <ProductList />
-                <OrderForm />
-            </main>
+        <div>
+            <div>
+                <CheckoutPage/>
+            </div>
         </div>
-    );
-};
+    )
+}
 
 export default App;
