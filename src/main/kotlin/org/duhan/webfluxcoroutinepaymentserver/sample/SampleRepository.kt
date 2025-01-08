daf9ad505ac03618ffced7db9273763aaf877d98
Duhan Mo <duhandv@gmail.com>
2025-01-09 00:41:01 +0900

    refactor: remove unused class

--- src/main/kotlin/org/duhan/webfluxcoroutinepaymentserver/sample/SampleRepository.kt
+++
@@ -1,5 +0,0 @@
-package org.duhan.webfluxcoroutinepaymentserver.sample
-
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-
-interface SampleRepository : CoroutineCrudRepository<Sample, Long>
